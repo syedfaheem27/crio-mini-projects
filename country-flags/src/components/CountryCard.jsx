0750cea7e1ed484ecd8146b72ec3a7b4214ef5ab
Syed Faheem <syedfaheem2719@gmail.com>
2024-03-24 19:32:02 +0530

    update alt text on images

--- country-flags/src/components/CountryCard.jsx
+++ country-flags/src/components/CountryCard.jsx
@@ -1,7 +1,7 @@
 const Card = ({ flag, name }) => {
   return (
     <div className="card">
-      <img src={flag} alt={name} />
+      <img src={flag} alt={`Flag of ${name}`} />
       <h4>{name}</h4>
     </div>
   );
