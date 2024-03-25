6a59a4681b7730eddbcac1006d488854088e78d2
syedfaheem27 <syedfaheem2719@gmail.com>
2024-03-25 17:57:30 +0530

    Add full name app

---
+++ fullName/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [fullName, setFullName] = useState("")
+
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    const formData = new FormData(e.target);
+
+    const firstName = formData.get("first");
+    const lastName = formData.get("last");
+
+    if (!firstName || !lastName)
+      return;
+
+    setFullName(`${firstName} ${lastName}`)
+  }
+  return (
+    <>
+      <header>
+        <h1>
+          Full Name Display
+        </h1>
+      </header>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="first">First Name:</label>
+          <input type="text" required id='first' name="first" />
+        </div>
+        <div>
+          <label htmlFor="last">Last Name:</label>
+          <input type="text" required id='last' name="last" />
+        </div>
+        <button>Submit</button>
+      </form>
+
+      <section className='full-name'>
+        {
+          fullName && <h4>
+            Full Name: {fullName}
+          </h4>
+        }
+      </section>
+    </>
+  )
+}
+
+export default App
